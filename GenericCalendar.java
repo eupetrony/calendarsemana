61ca944b78d32cd9192f78e3c94453b433d9af09
Petrony Pimentel <eupetrony@gmail.com>
2017-06-05 11:28:02 -0300

    Update GenericCalendar.java

--- GenericCalendar.java
+++ GenericCalendar.java
@@ -6,6 +6,31 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 public class GenericCalendar {
+    /**
+     * Retorna ano mes e dia sem o caractere /
+     * 
+     * @param calendar
+     * @return String
+     * @throws Exception
+     */
+    public static String getYYYYMMMDD(Calendar calendar) throws Exception {
+        SimpleDateFormat formataData = new SimpleDateFormat("yyyyMMdd");
+        String anoFormatado = formataData.format(calendar.getTime());
+        return anoFormatado;
+    }
+    
+    /**
+     * Retorna ano Ano e mes sem o caractere /
+     * @param calendar
+     * @return String
+     * @throws Exception 
+     */
+    public static String getYYYYMMM(Calendar calendar) throws Exception {
+        SimpleDateFormat formataData = new SimpleDateFormat("yyyyMM");
+        String anoFormatado = formataData.format(calendar.getTime());
+        return anoFormatado;
+    }
+    
     public static Calendar getInicioSemana(Calendar dataAtual) throws Exception {
         GregorianCalendar c = new GregorianCalendar();
         c.setFirstDayOfWeek(Calendar.MONDAY);
